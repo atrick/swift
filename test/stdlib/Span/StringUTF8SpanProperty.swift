05b833f9f999bff042f5302587aebe7295aae6a9
Andrew Trick <atrick@apple.com>
2025-03-19 14:01:07 -0700

    Revert "[temporary] disable small-string support"
    This reverts commit 188f1a03f5770e48641ae70c4729d680a441712b.

--- test/stdlib/Span/StringUTF8SpanProperty.swift
+++ test/stdlib/Span/StringUTF8SpanProperty.swift
@@ -20,7 +20,6 @@ var suite = TestSuite("StringUTF8StorageProperty")
 defer { runAllTests() }
 
 suite.test("Span from Small String")
-  .skip(.always("Support inline-stored Spans forthcoming"))
 .require(.stdlib_6_2).code {
   guard #available(SwiftStdlib 6.2, *) else { return }
 
@@ -53,7 +52,6 @@ suite.test("Span from Large Native String")
 }
 
 suite.test("Span from Small String's Substring")
-.skip(.always("Support inline-stored Spans forthcoming"))
 .require(.stdlib_6_2).code {
   guard #available(SwiftStdlib 6.2, *) else { return }
 
